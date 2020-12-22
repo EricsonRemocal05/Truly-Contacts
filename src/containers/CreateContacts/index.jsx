9506c6d5ee1ba0e24d679f20ff3d523a150b7c57
EricsonRemocal05 <remocalericson05@gmail.com>
2020-12-22 18:11:23 +0800

    added the reusable header component

--- src/containers/CreateContacts/index.jsx
+++ src/containers/CreateContacts/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../../components/Header';
 
 const CreatContactContainer = () => {
   return (
     <div>
+      <Header />
       <h1>CreatContact</h1>
     </div>
   );
