29f21255f72b0b2fa7e875d658917ff275ad69c1
EricsonRemocal05 <remocalericson05@gmail.com>
2020-12-22 11:34:32 +0800

    setting up semantic UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './assets/logo.svg';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import routes from './routes';
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
 function App() {
