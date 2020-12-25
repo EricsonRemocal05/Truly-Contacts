ce469ab5d03855d91bdac4f4f69c364ed345bf7c
EricsonRemocal05 <remocalericson05@gmail.com>
2020-12-25 12:53:01 +0800

    added route protection

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import logo from './assets/logo.svg';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useHistory,
+} from 'react-router-dom';
 import routes from './routes';
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 import { GlobalProvider } from './context/Provider';
+import isAuthenticated from './utils/isAuthenticated';
+
+const RenderRoute = (route) => {
+  const history = useHistory();
+
+  if (route.needsAuth && !isAuthenticated()) {
+    history.push('/auth/login');
+  }
+
+  return (
+    <Route
+      path={route.path}
+      exact
+      render={(props) => <route.component {...props} />}
+    ></Route>
+  );
+};
 
 function App() {
   return (
@@ -11,12 +33,7 @@ function App() {
       <Router>
         <Switch>
           {routes.map((route, index) => (
-            <Route
-              key={index}
-              path={route.path}
-              exact
-              render={(props) => <route.component {...props} />}
-            ></Route>
+            <RenderRoute {...route} key={index} />
           ))}
         </Switch>
       </Router>
